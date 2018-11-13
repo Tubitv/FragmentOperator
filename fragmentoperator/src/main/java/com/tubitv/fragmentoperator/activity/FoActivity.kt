5d94511086cf88feff53b2f699ef5729ce979063
Heming Liu <liuhm1113@gmail.com>
2018-11-12 16:14:54 -0800

    Fix prod crashes when load fragment after activity onSaveInstanceState

--- fragmentoperator/src/main/java/com/tubitv/fragmentoperator/activity/FoActivity.kt
+++ fragmentoperator/src/main/java/com/tubitv/fragmentoperator/activity/FoActivity.kt
@@ -6,25 +6,38 @@ import android.support.v7.app.AppCompatActivity
 abstract class FoActivity : AppCompatActivity() {
     private val TAG = FoActivity::class.simpleName
 
-    private var isForeground = false
+    private var mIsForeground = false
+    private var mFragmentManagerPrepared = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        isForeground = true
+        mIsForeground = true
+        mFragmentManagerPrepared = true
     }
 
     override fun onResume() {
         super.onResume()
-        isForeground = true
+        mIsForeground = true
+        mFragmentManagerPrepared = true
     }
 
     override fun onPause() {
         super.onPause()
-        isForeground = false
+        mIsForeground = false
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mFragmentManagerPrepared = false
+    }
+
+
     fun isForeground(): Boolean {
-        return isForeground
+        return mIsForeground
+    }
+
+    fun isReadyForFragmentOperation(): Boolean {
+        return mIsForeground && mFragmentManagerPrepared
     }
 
     abstract fun getFragmentContainerResId(): Int
