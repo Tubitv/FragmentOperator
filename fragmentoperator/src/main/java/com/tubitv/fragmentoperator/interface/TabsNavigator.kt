b522ad6d3b494801c51637b4ed85adb97fbc5f18
Heming Liu <liuhm1113@gmail.com>
2018-11-28 17:13:39 -0800

    Finish all child fragment tab navigation

--- fragmentoperator/src/main/java/com/tubitv/fragmentoperator/interface/TabsNavigator.kt
+++ fragmentoperator/src/main/java/com/tubitv/fragmentoperator/interface/TabsNavigator.kt
@@ -1,5 +1,14 @@
 package com.tubitv.fragmentoperator.`interface`
 
+import com.tubitv.fragmentoperator.fragment.FoFragment
+
 interface TabsNavigator {
+
+    fun getCurrentTabIndex(): Int
+
+    fun getCurrentContainerFragment(): FoFragment?
+
+    fun isTabIndexValid(tabIndex: Int): Boolean
     
+    fun switchToTab(index: Int)
 }
